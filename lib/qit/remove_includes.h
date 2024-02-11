b5e227b50461cd7b7133ebc78a3afc922aea16a1
windraak <80174115+windraak@users.noreply.github.com>
2024-02-11 12:27:20 +0100

    Remind arduino ide users to trim the automatically included libraries

---
+++ lib/qit/remove_includes.h
@@ -0,0 +1,6 @@
+
+// A trick so Arduino IDE users are reminded to only include <Automatic.h> or <AutomaticStated.h>
+
+#warning !!!
+#warning Please remove all qit includes except <Automatic.h> or <AutomaticStated.h>
+#warning !!!
